5886444c9a3765be9d0c42a66d0738a155814550
Kyle Anson <kyle.anson7@gmail.com>
2020-11-25 13:57:43 -0500

    Gif update

--- commands/war.py
+++ commands/war.py
@@ -6,7 +6,11 @@ from datetime import date, datetime
 
 from config import Config
 
+from utils.attack_reactions import Attack_Reactions
+
 def create_war_commands(config:Config, bot:Bot, coc_client:EventsClient):
+  attack_reactions = Attack_Reactions(config)
+
   @bot.group(invoke_without_command=True)
   @commands.guild_only()
   @commands.has_permissions(administrator=True)
@@ -22,6 +26,23 @@ def create_war_commands(config:Config, bot:Bot, coc_client:EventsClient):
     # Grab our war log
     # Grab enemy war log
 
+  @war.command(name='test')
+  @commands.guild_only()
+  @commands.has_permissions(administrator=True)
+  async def  test(ctx:Context, attacker_name:str, defender_name:str):
+    gif = attack_reactions.random_good_gif('', '')
+    await bot.get_channel(config.coc_channel).send(attack_reactions.random_good_line(
+      WarMemberMock(attacker_name),
+      WarMemberMock(defender_name)
+    ))
+    await bot.get_channel(config.coc_channel).send(gif)
+
 # Analyze list of wars
 def analyze_war_log(war_log:list):
   pass
+
+class WarMemberMock():
+  name:str
+
+  def __init__(self, name):
+    self.name = name
