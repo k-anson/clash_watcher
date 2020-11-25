8fe264ee43ec24643284b6eab54e27537e7a6133
Kyle Anson <kyle.anson7@gmail.com>
2020-11-25 14:55:27 -0500

    Fix channel references

--- coc_events/war.py
+++ coc_events/war.py
@@ -1,4 +1,5 @@
 from discord.ext.commands import Bot
+from discord import TextChannel
 import coc
 from coc import EventsClient, ClanWar, WarAttack
 
@@ -11,13 +12,19 @@ def create_war_events(config:Config, bot:Bot, coc_client:EventsClient):
   @coc_client.event
   @coc.WarEvents.state(tags=config.clan_tag)
   async def on_war_state_change(old_war:ClanWar, new_war:ClanWar):
-    bot.get_channel(config.coc_channel).send(f'War status changed: {new_war.status}')
+    coc_channel = bot.get_channel(config.coc_channel)
+
+    await coc_channel.send(f'War status changed: {new_war.status}')
 
   @coc_client.event
   @coc.WarEvents.war_attack(tags=config.clan_tag)
   async def on_war_attack(attack:WarAttack, war:ClanWar):
+    coc_channel = bot.get_channel(config.coc_channel)
+
     # Attacker is from our clan
     if (attack.attacker.clan.tag == config.clan_tag):
-      bot.get_channel(config.coc_channel).send(attack_reactions.random_good_line(attack.attacker, attack.defender))
+      gif = attack_reactions.random_good_gif(attack.attacker, attack.defender)
+      await coc_channel.send(attack_reactions.random_good_line(attack.attacker, attack.defender))
+      await coc_channel.send(gif)
     else:
-      bot.get_channel(config.coc_channel).send(f'{attack.defender.name} defended against {attack.attacker.name}')
+      await coc_channel.send(f'{attack.defender.name} defended against {attack.attacker.name}')
